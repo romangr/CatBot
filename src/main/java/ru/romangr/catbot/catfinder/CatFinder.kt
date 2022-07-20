0e2ec48a04d928eff963396a4cbb1fd8d7a71c5d
romangr <romangr@users.noreply.github.com>
2022-07-20 21:56:19 +0400

    Fix invalid links with cats (#21)

--- src/main/java/ru/romangr/catbot/catfinder/CatFinder.kt
+++ src/main/java/ru/romangr/catbot/catfinder/CatFinder.kt
@@ -1,11 +1,9 @@
 package ru.romangr.catbot.catfinder
 
 import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
 import org.springframework.core.ParameterizedTypeReference
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
-import org.springframework.http.RequestEntity
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
 import org.springframework.web.client.RestTemplate
 import ru.romangr.catbot.utils.URLBuilder
 import ru.romangr.exceptional.Exceptional
@@ -21,22 +19,48 @@ class CatFinder {
     private val requestEntity: RequestEntity<Void>
 
     val cat: Exceptional<Cat>
-        get() = Exceptional
-                .getExceptional<ResponseEntity<List<Cat>>> {
-                    restTemplate.exchange(requestEntity,
-                            object : ParameterizedTypeReference<List<Cat>>() {
+        get() {
+            var result: Exceptional<Cat> = Exceptional.empty()
+            for (i in 1..RETRY_NUMBER) {
+                result = retrieveCat()
+                result.ifException { log.warn("Exception during cat retrieval", it) }
+                if (result.isValuePresent) {
+                    return result
+                }
+            }
+            return result
+        }
 
-                            })
-                }
-                .safelyMap {
-                    if (it.statusCode != HttpStatus.OK) {
-                        val message = "Unexpected response from Cat API: ${it.statusCode}\n${it.headers}\n${it.body}"
-                        throw RuntimeException(message)
+    private fun retrieveCat(): Exceptional<Cat> =
+            Exceptional.getExceptional<ResponseEntity<List<Cat>>> {
+                restTemplate.exchange(requestEntity, object : ParameterizedTypeReference<List<Cat>>() {
+
+                })
+            }
+                    .safelyMap {
+                        if (it.statusCode != HttpStatus.OK) {
+                            val message = "Unexpected response from Cat API: ${it.statusCode}\n${it.headers}\n${it.body}"
+                            throw RuntimeException(message)
+                        }
+                        return@safelyMap it
                     }
-                    return@safelyMap it
-                }
-                .map { it.body }
-                .map { list -> list?.get(0) }
+                    .map { it.body }
+                    .map { list -> list?.get(0) }
+                    .safelyMap {
+                        var response = restTemplate.exchange(it!!.url, HttpMethod.HEAD, null, String::class.java)
+                        if (response.statusCode === HttpStatus.MOVED_PERMANENTLY) {
+                            val location = response.headers.location
+                            if (location == null) {
+                                throw RuntimeException("No location along with redirect response to ${it.url}");
+                            }
+                            response = restTemplate.exchange(location, HttpMethod.HEAD, null, String::class.java)
+                        }
+                        val contentType = response.headers.contentType
+                        if (contentType != null && SUPPORTED_CONTENT_TYPES.contains(contentType)) {
+                            return@safelyMap it;
+                        }
+                        throw RuntimeException("Unsupported content type: $contentType");
+                    }
 
     constructor(restTemplate: RestTemplate) {
         this.restTemplate = restTemplate
@@ -46,23 +70,22 @@ class CatFinder {
 
     constructor(restTemplate: RestTemplate, apiKey: String) {
         this.restTemplate = restTemplate
-        val url = URI.create(prepareUrlWithoutApiKey()
-                .withParameter("api_key", apiKey)
-                .build())
+        val url = URI.create(prepareUrlWithoutApiKey().withParameter("api_key", apiKey).build())
         requestEntity = getRequestEntity(url)
     }
 
 
     private fun prepareUrlWithoutApiKey(): URLBuilder {
-        return URLBuilder()
-                .withHost("https://api.thecatapi.com")
-                .withPath("api/images/get")
-                .withParameter("format", "json")
+        return URLBuilder().withHost("https://api.thecatapi.com").withPath("api/images/get").withParameter("format", "json")
     }
 
     private fun getRequestEntity(url: URI): RequestEntity<Void> {
-        return RequestEntity.get(url)
-                .accept(MediaType.APPLICATION_JSON_UTF8)
-                .build()
+        return RequestEntity.get(url).accept(MediaType.APPLICATION_JSON_UTF8).build()
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(CatFinder::class.java)
+        private val SUPPORTED_CONTENT_TYPES: List<MediaType> = listOf(MediaType.IMAGE_GIF, MediaType.IMAGE_JPEG, MediaType.IMAGE_PNG)
+        private const val RETRY_NUMBER = 3
     }
 }
--- src/test/java/ru/romangr/catbot/catfinder/CatFinderTest.java
+++ src/test/java/ru/romangr/catbot/catfinder/CatFinderTest.java
@@ -1,6 +1,8 @@
 package ru.romangr.catbot.catfinder;
 
+import java.net.URI;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +10,7 @@ import ru.romangr.exceptional.Exceptional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 /**
@@ -15,19 +18,79 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
  */
 class CatFinderTest {
 
-    private RestTemplate restTemplate = new RestTemplate();
-    private CatFinder catFinder = new CatFinder(restTemplate);
+  private RestTemplate restTemplate = new RestTemplate();
+  private CatFinder catFinder = new CatFinder(restTemplate);
 
-    @Test
-    void getCatSuccessfully() {
-        MockRestServiceServer.createServer(restTemplate)
-                .expect(requestTo("https://api.thecatapi.com/api/images/get?format=json"))
-                .andRespond(withSuccess("[{\"url\": \"https://example.com/test.jpg\"}]",
-                        MediaType.APPLICATION_JSON_UTF8));
+  @Test
+  void getCatSuccessfully() {
+    MockRestServiceServer server = MockRestServiceServer.createServer(restTemplate);
+    server.expect(requestTo("https://api.thecatapi.com/api/images/get?format=json"))
+        .andRespond(withSuccess("[{\"url\": \"https://example.com/test.jpg\"}]",
+            MediaType.APPLICATION_JSON_UTF8));
+    server.expect(requestTo("https://example.com/test.jpg"))
+        .andRespond(withSuccess().contentType(MediaType.IMAGE_JPEG));
 
-        Exceptional<Cat> cat = catFinder.getCat();
+    Exceptional<Cat> cat = catFinder.getCat();
 
-        assertThat(cat.isValuePresent()).isTrue();
-        assertThat(cat.getValue().getUrl()).isEqualTo("https://example.com/test.jpg");
-    }
+    assertThat(cat.isValuePresent()).isTrue();
+    assertThat(cat.getValue().getUrl()).isEqualTo("https://example.com/test.jpg");
+  }
+
+  @Test
+  void getCatSuccessfullyAfterRetryWithUnexpectedContentType() {
+    MockRestServiceServer server = MockRestServiceServer.createServer(restTemplate);
+    server.expect(requestTo("https://api.thecatapi.com/api/images/get?format=json"))
+        .andRespond(withSuccess("[{\"url\": \"https://example.com/test.jpg\"}]",
+            MediaType.APPLICATION_JSON_UTF8));
+    server.expect(requestTo("https://example.com/test.jpg"))
+        .andRespond(withSuccess().contentType(MediaType.APPLICATION_XML));
+    server.expect(requestTo("https://api.thecatapi.com/api/images/get?format=json"))
+        .andRespond(withSuccess("[{\"url\": \"https://example.com/test2.jpg\"}]",
+            MediaType.APPLICATION_JSON_UTF8));
+    server.expect(requestTo("https://example.com/test2.jpg"))
+        .andRespond(withSuccess().contentType(MediaType.IMAGE_JPEG));
+
+    Exceptional<Cat> cat = catFinder.getCat();
+
+    assertThat(cat.isValuePresent()).isTrue();
+    assertThat(cat.getValue().getUrl()).isEqualTo("https://example.com/test2.jpg");
+  }
+
+  @Test
+  void getCatSuccessfullyAfterRetryWithUnexpectedStatusCode() {
+    MockRestServiceServer server = MockRestServiceServer.createServer(restTemplate);
+    server.expect(requestTo("https://api.thecatapi.com/api/images/get?format=json"))
+        .andRespond(withSuccess("[{\"url\": \"https://example.com/test.jpg\"}]",
+            MediaType.APPLICATION_JSON_UTF8));
+    server.expect(requestTo("https://example.com/test.jpg"))
+        .andRespond(withStatus(HttpStatus.NOT_FOUND));
+    server.expect(requestTo("https://api.thecatapi.com/api/images/get?format=json"))
+        .andRespond(withSuccess("[{\"url\": \"https://example.com/test2.jpg\"}]",
+            MediaType.APPLICATION_JSON_UTF8));
+    server.expect(requestTo("https://example.com/test2.jpg"))
+        .andRespond(withSuccess().contentType(MediaType.IMAGE_JPEG));
+
+    Exceptional<Cat> cat = catFinder.getCat();
+
+    assertThat(cat.isValuePresent()).isTrue();
+    assertThat(cat.getValue().getUrl()).isEqualTo("https://example.com/test2.jpg");
+  }
+
+  @Test
+  void getCatSuccessfullyWithRedirect() {
+    MockRestServiceServer server = MockRestServiceServer.createServer(restTemplate);
+    server.expect(requestTo("https://api.thecatapi.com/api/images/get?format=json"))
+        .andRespond(withSuccess("[{\"url\": \"https://example.com/test.jpg\"}]",
+            MediaType.APPLICATION_JSON_UTF8));
+    server.expect(requestTo("https://example.com/test.jpg"))
+        .andRespond(withStatus(HttpStatus.MOVED_PERMANENTLY).location(
+            URI.create("https://example.com/test2.jpg")).contentType(MediaType.TEXT_HTML));
+    server.expect(requestTo("https://example.com/test2.jpg"))
+        .andRespond(withSuccess().contentType(MediaType.IMAGE_JPEG));
+
+    Exceptional<Cat> cat = catFinder.getCat();
+
+    assertThat(cat.isValuePresent()).isTrue();
+    assertThat(cat.getValue().getUrl()).isEqualTo("https://example.com/test.jpg");
+  }
 }
