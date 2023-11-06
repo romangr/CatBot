6b83ce61f48d7b21308cd50f3a01dbcf862520f4
Roman Golovin <roman_gr@mail.com>
2023-11-06 19:07:07 +0200

    Add number of subscribers to statistics

--- src/main/java/ru/romangr/catbot/handler/StatisticsCommandHandler.kt
+++ src/main/java/ru/romangr/catbot/handler/StatisticsCommandHandler.kt
@@ -3,12 +3,14 @@ package ru.romangr.catbot.handler
 import ru.romangr.catbot.executor.action.TelegramAction
 import ru.romangr.catbot.executor.action.TelegramActionFactory
 import ru.romangr.catbot.statistic.StatisticService
+import ru.romangr.catbot.subscription.SubscribersService
 import ru.romangr.catbot.telegram.model.Chat
 
 @StaticCommand(BotCommand.STATISTICS)
 class StatisticsCommandHandler(private val actionFactory: TelegramActionFactory,
                                private val adminChatId: Long?,
-                               statisticService: StatisticService)
+                               statisticService: StatisticService,
+                               private val subscribersService: SubscribersService)
     : StaticCommandHandler(statisticService) {
 
     override fun handleStringCommand(chat: Chat, text: String): List<TelegramAction> {
@@ -18,6 +20,7 @@ class StatisticsCommandHandler(private val actionFactory: TelegramActionFactory,
         val (sinceDate, stats) = statisticService.commandStatistics()
         val messageTextBuilder = StringBuilder("Action statistics since $sinceDate:")
         stats.forEach { (action, value) -> messageTextBuilder.append("\n  $action: $value") }
+        messageTextBuilder.append("\n  Subscribers: ${subscribersService.subscribersCount}")
         return listOf(actionFactory.newSendMessageAction(chat, messageTextBuilder.toString()))
     }
 }
